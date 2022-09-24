7ec7eafc50b92d11ee06fd38f8cde66ebf7914c8
Revincx <revincx@outlook.com>
2022-09-24 15:01:31 +0800

    Force overwrite package files

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+cd $GITHUB_WORKSPACE/$VENDOR-imagebuilder-$VERSION-x86-64.Linux-x86_64 || exit
+
 # Remove redundant default packages
 sed -i "/luci-app-cpufreq/d" include/target.mk
+
+# Force opkg to overwrite files
+sed -i "s/install \$(BUILD_PACKAGES)/install \$(BUILD_PACKAGES) --force-overwrite/" Makefile
