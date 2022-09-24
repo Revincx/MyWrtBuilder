68ff7e5c3202764ab40d791ee2b1de3a6e0293c3
Revincx <revincx@outlook.com>
2022-09-24 15:34:59 +0800

    Do not generate ISO and VHDX images

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -7,3 +7,9 @@ sed -i "/luci-app-cpufreq/d" include/target.mk
 
 # Force opkg to overwrite files
 sed -i "s/install \$(BUILD_PACKAGES)/install \$(BUILD_PACKAGES) --force-overwrite/" Makefile
+
+# Not generate ISO images for it is too big
+sed -i "s/CONFIG_ISO_IMAGES=y/# CONFIG_ISO_IMAGES is not set/" .config
+
+# Not generate VHDX images
+sed -i "s/CONFIG_VHDX_IMAGES=y/# CONFIG_VHDX_IMAGES is not set/" .config
