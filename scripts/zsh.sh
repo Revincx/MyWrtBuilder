c8e9298921ecda3e798174ebd7ac943e32f27e02
Revincx <revincx233@gmail.com>
2022-10-09 22:56:34 +0800

    Add oh-my-zsh

---
+++ scripts/zsh.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Start oh-my-zsh Config !"
+echo "Current Path: $PWD"
+
+mkdir -p $PWD/files/root/.oh-my-zsh
+
+ROOT=$PWD/files/root
+ZSH=$ROOT/.oh-my-zsh
+ZSH_CUSTOM=$ZSH/custom
+
+git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git $ZSH
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/zsh-autosuggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/zsh-syntax-highlighting
+
+cp $ZSH/templates/zshrc.zsh-template $ROOT/.zshrc
+
+sed -i "s/ZSH_THEME=\".\+\"/ZSH_THEME=\"ys\"/" $ROOT/.zshrc
+
+sed -i "s/plugins=.\+/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/" $ROOT/.zshrc
