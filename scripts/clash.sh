4297f57a559b36280fef59ff1dac24a226dcc248
Revincx <revincx233@gmail.com>
2022-10-10 11:29:21 +0800

    fix: clash kernel does not download

--- scripts/clash.sh
+++ scripts/clash.sh
@@ -4,7 +4,7 @@ echo "Start Clash Core Download !"
 echo "Current Path: $PWD"
 
 mkdir -p files/etc/openclash/core
-cd files/etc/openclash/core || echo "Clash core path does not exist! " && exit
+cd files/etc/openclash/core || (echo "Clash core path does not exist! " && exit)
 wget https://raw.githubusercontent.com/vernesong/OpenClash/master/core-lateset/dev/clash-linux-amd64.tar.gz
 tar -zxvf clash-linux-amd64.tar.gz
 rm -rf clash-linux-amd64.tar.gz
