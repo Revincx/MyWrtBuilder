4f2e688901166120285ae225171b830ff6746e4f
星野 <153903925+XingYeNeko@users.noreply.github.com>
2024-01-23 12:29:14 +0800

    Update clash.sh
    更新open clash内核下载

--- scripts/clash.sh
+++ scripts/clash.sh
@@ -5,6 +5,6 @@ echo "Current Path: $PWD"
 
 mkdir -p files/etc/openclash/core
 cd files/etc/openclash/core || (echo "Clash core path does not exist! " && exit)
-wget https://raw.githubusercontent.com/vernesong/OpenClash/master/core-lateset/dev/clash-linux-amd64.tar.gz
+wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz
 tar -zxvf clash-linux-amd64.tar.gz
 rm -rf clash-linux-amd64.tar.gz
