78cc9d5d6cbc8502d39870b688c26d9e2bb02b7e
Revincx <revincx233@gmail.com>
2024-01-23 16:50:16 +0800

    improve: download all clash cores

--- scripts/clash.sh
+++ scripts/clash.sh
@@ -5,6 +5,24 @@ echo "Current Path: $PWD"
 
 mkdir -p files/etc/openclash/core
 cd files/etc/openclash/core || (echo "Clash core path does not exist! " && exit)
+
+# Clash Dev
+wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-amd64.tar.gz
+tar -zxvf clash-linux-amd64.tar.gz
+rm -rf clash-linux-amd64.tar.gz
+mv clash clash_dev
+
+# Clash TUN
+wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/premium/clash-linux-amd64.tar.gz
+tar -zxvf clash-linux-amd64.tar.gz
+rm -rf clash-linux-amd64.tar.gz
+mv clash clash_tun
+
+# Clash Meta
 wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz
 tar -zxvf clash-linux-amd64.tar.gz
 rm -rf clash-linux-amd64.tar.gz
+mv clash clash_meta
+
+# Use clash_dev as default core
+mv clash_dev clash
