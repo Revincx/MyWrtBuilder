49c44c8842b694035ed4f385b5610db9ced93ae0
Revincx <revincx233@gmail.com>
2024-01-25 22:00:14 +0800

    fix: clash premium core download link

--- scripts/clash.sh
+++ scripts/clash.sh
@@ -13,10 +13,11 @@ rm -rf clash-linux-amd64.tar.gz
 mv clash clash_dev
 
 # Clash TUN
-wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/premium/clash-linux-amd64.tar.gz
-tar -zxvf clash-linux-amd64.tar.gz
-rm -rf clash-linux-amd64.tar.gz
-mv clash clash_tun
+VERSION=$(curl -sS https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version | awk 'NR==2')
+wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/premium/clash-linux-amd64-$VERSION.gz
+gzip -d clash-linux-amd64-$VERSION.gz
+rm -rf clash-linux-amd64-$VERSION.gz
+mv clash-linux-amd64-$VERSION clash_tun
 
 # Clash Meta
 wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz
